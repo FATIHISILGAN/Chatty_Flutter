b36a79fbba6463fc086e421155a7ccda2803bafb
Fatih ışılgan <fatihisilgan@hotmail.com>
2020-09-27 21:01:23 +0300

    login,register and forgot password done

--- lib/sayfalar/login.dart
+++ lib/sayfalar/login.dart
@@ -1,6 +1,9 @@
-import 'package:chaty/sayfalar/forgotpassword.dart';
+
 import 'package:chaty/sayfalar/register.dart';
+
 import 'package:flutter/material.dart';
+
+import 'forgotpassword.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -11,8 +14,8 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
-      body: Container(
+        resizeToAvoidBottomPadding: false,
+        body: Container(
           width: double.infinity,
           decoration: BoxDecoration(
             gradient: LinearGradient(
@@ -20,124 +23,126 @@ class _LoginPageState extends State<LoginPage> {
               begin: FractionalOffset(0.3, 1),
             ),
           ),
-          child: Flexible(
-            child: Container(
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 80,
+          child: Container(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 80,
+                ),
+                Flexible(
+                  child: Container(
+                      height: 200,
+                      width: 200,
+                      child: Image.network(
+                          "https://images.vexels.com/media/users/3/136808/isolated/preview/d3455a22af5f3ed7565fb5fb71bb8d43-send-message-icon-by-vexels.png")),
+                ),
+                Container(
+                  child: Text(
+                    "Login",
+                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
                   ),
-                  Flexible(
-                    child: Container(
-                        height: 200,
-                        width: 200,
-                        child: Image.network(
-                            "https://images.vexels.com/media/users/3/136808/isolated/preview/d3455a22af5f3ed7565fb5fb71bb8d43-send-message-icon-by-vexels.png")),
-                  ),
-                  Flexible(
-                    child: Container(
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                            fontSize: 50, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    child: Flexible(
-                      child: Form(
-                        child: Column(
-                          children: [
-                            Flexible(
-                              child: Container(
-                                width: 275,
-                                child: TextFormField(
-                                  decoration: InputDecoration(
-                                    labelText: "Email",
-                                    hintText: "example@example.com",
-                                    isDense: true,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Flexible(
-                              child: Container(
-                                width: 275,
-                                child: TextFormField(
-                                  decoration: InputDecoration(
-                                    labelText: "Parola",
-                                    hintText: "******",
-                                    isDense: true,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Flexible
-                              (
-
-                              child: FlatButton(onPressed: (){
-                                Navigator.push(context, MaterialPageRoute(builder: (context)=>ForgotPassword()));
-                              },child: Text("Şifremi Unuttum",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 12,color: Colors.white),),),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Flexible(
-                    child: Container(
-                      child: RaisedButton(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                              180,
-                            ),
-                            side: BorderSide(color: Colors.white)),
-                        color: Colors.amberAccent[100],
-                        child: Text(
-                          "Giriş Yap",
-                          style: TextStyle(
-                              fontSize: 15, fontWeight: FontWeight.bold),
-                        ),
-                        onPressed: () {},
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Flexible(
-                    child: Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                Container(
+                  child: Flexible(
+                    child: Form(
+                      child: Column(
                         children: [
-                          Text(
-                            "Üye değil misiniz ?",
-                            style:
-                                TextStyle(fontSize: 20, color: Colors.blueGrey),
+                          Container(
+                            width: 275,
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                labelText: "Email",
+                                hintText: "example@example.com",
+                                isDense: true,
+                              ),
+                            ),
                           ),
-                          FlatButton(
-                              onPressed: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => RegisterPage()));
-                              },
-                              child: Text(
-                                "Hemen Ol !",
-                                style:
-                                    TextStyle(fontSize: 25, color: Colors.red),
-                              ))
+                          Container(
+                            width: 275,
+                            child: TextFormField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                labelText: "Parola",
+                                hintText: "******",
+                                isDense: true,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 2),
+                          Flexible(
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 185),
+                              child: FlatButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              ForgotPassword()));
+                                },
+                                child: Text(
+                                  "Şifremi Unuttum",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 12,
+                                      color: Colors.white),
+                                ),
+                              ),
+                            ),
+                          ),
                         ],
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          180,
+                        ),
+                        side: BorderSide(color: Colors.white)),
+                    color: Colors.amberAccent[100],
+                    child: Text(
+                      "Giriş Yap",
+                      style:
+                      TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Flexible(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Üye değil misiniz ?",
+                        style: TextStyle(fontSize: 14, color: Colors.white),
+                      ),
+                      FlatButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => RegisterPage()));
+                          },
+                          child: Text(
+                            "Hemen Ol !",
+                            style: TextStyle(
+                                color: Colors.amberAccent, fontSize: 20),
+                          ))
+                    ],
+                  ),
+                ),
+              ],
             ),
-          )),
-    );
+          ),
+        ));
   }
 }
--- lib/sayfalar/register.dart
+++ lib/sayfalar/register.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,8 +13,8 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
-      body: Container(
+        resizeToAvoidBottomPadding: false,
+        body: Container(
           width: double.infinity,
           decoration: BoxDecoration(
             gradient: LinearGradient(
@@ -21,117 +22,107 @@ class _RegisterPageState extends State<RegisterPage> {
               begin: FractionalOffset(0.3, 1),
             ),
           ),
-          child: Flexible(
-            child: Container(
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 80,
-                  ),
-                  Flexible(
-                    child: Container(
-                        height: 200,
-                        width: 200,
-                        child: Image.network(
-                            "https://images.vexels.com/media/users/3/136808/isolated/preview/d3455a22af5f3ed7565fb5fb71bb8d43-send-message-icon-by-vexels.png")),
-                  ),
-                  Flexible(
-                    child: Container(
-                      child: Text(
-                        "Register",
-                        style: TextStyle(
-                            fontSize: 50, fontWeight: FontWeight.bold),
-                      ),
+          child: Container(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 80,
+                ),
+                Flexible(
+                  child: Container(
+                      height: 200,
+                      width: 200,
+                      child: Image.network(
+                          "https://images.vexels.com/media/users/3/136808/isolated/preview/d3455a22af5f3ed7565fb5fb71bb8d43-send-message-icon-by-vexels.png")),
+                ),
+                Flexible(
+                  child: Container(
+                    child: Text(
+                      "Register",
+                      style: TextStyle(
+                          fontSize: 50, fontWeight: FontWeight.bold),
                     ),
                   ),
-                  Container(
-                    child: Flexible(
-                      child: Form(
-                        child: Column(
-                          children: [
-                            Flexible(
-                              child: Container(
-                                width: 275,
-                                child: TextFormField(
-                                  decoration: InputDecoration(
-                                    labelText: "Email",
-                                    hintText: "example@example.com",
-                                    isDense: true,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Flexible(
-                              child: Container(
-                                width: 275,
-                                child: TextFormField(
-                                  decoration: InputDecoration(
-                                    labelText: "Parola",
-                                    hintText: "******",
-                                    isDense: true,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Flexible(
-                    child: Container(
-                      child: RaisedButton(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                              180,
-                            ),
-                            side: BorderSide(color: Colors.white)),
-                        color: Colors.amberAccent[100],
-                        child: Text(
-                          "Kayıt Ol",
-                          style: TextStyle(
-                              fontSize: 15, fontWeight: FontWeight.bold),
-                        ),
-                        onPressed: () {},
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Flexible(
-                    child: Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                Container(
+                  child: Flexible(
+                    child: Form(
+                      child: Column(
                         children: [
-                          Text(
-                            "Zaten Üye misiniz ?",
-                            style:
-                                TextStyle(fontSize: 20, color: Colors.blueGrey),
+                          Container(
+                            width: 275,
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                labelText: "Email",
+                                hintText: "example@example.com",
+                                isDense: true,
+                              ),
+                            ),
                           ),
-                          FlatButton(
-                              onPressed: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => LoginPage()));
-                              },
-                              child: Text(
-                                "Giriş Yap !",
-                                style:
-                                    TextStyle(fontSize: 25, color: Colors.red),
-                              ))
+
+                          Container(
+                            width: 275,
+                            child: TextFormField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                labelText: "Parola",
+                                hintText: "******",
+                                isDense: true,
+                              ),
+                            ),
+                          ),
+
                         ],
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+                SizedBox(height: 10,),
+                Flexible(
+                  child: Container(
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                            180,
+                          ),
+                          side: BorderSide(color: Colors.white)),
+                      color: Colors.amberAccent[100],
+                      child: Text(
+                        "Kayıt Ol",
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Flexible(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Zaten Üye misiniz ?",
+                        style: TextStyle(fontSize: 14, color: Colors.white),
+                      ),
+                      FlatButton(
+                          onPressed: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginPage()));
+                          },
+                          child: Text(
+                            "Giriş Yap !",
+                            style: TextStyle(color: Colors.amberAccent,fontSize: 20),
+                          ))
+                    ],
+                  ),
+                ),
+
+              ],
             ),
-          )),
+          ),
+        )
     );
   }
 }
